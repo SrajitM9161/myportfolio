7ed426e15d9cab70beb200f145da571b0d87ee09
Srajit Mishra <106705809+SrajitM9161@users.noreply.github.com>
2024-12-17 20:22:12 +0530

    Update page.jsx

--- OneDrive/Desktop/My_srajit/myport/app/resume/page.jsx
+++ OneDrive/Desktop/My_srajit/myport/app/resume/page.jsx
@@ -5,7 +5,7 @@ const Resume = () => {
   return (
     <div className="flex justify-center items-center h-screen">
       <motion.iframe
-       src="https://drive.google.com/embeddedfolderview?id=1FOduXOuZfJxV9c7fDkApUYPkGoh_o3di#grid"
+       src="https://drive.google.com/file/d/1iB2cz5zYWCl5e03uTjPRW37HBSSaIoAK/preview"
         className="border-4 border-blue-500 rounded-lg"
         initial={{ opacity: 0, scale: 0.5 }}
         animate={{ opacity: 1, scale: 1 }}
